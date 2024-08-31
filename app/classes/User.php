13ff6ab9da2e9842907dfd59487fa3da758e16b6
dejandurlevic <dejandurlevic@hotmail.com>
2024-08-31 23:32:10 +0200

    update

--- app/classes/User.php
+++ app/classes/User.php
@@ -11,11 +11,11 @@ class User {
     public function register($username, $email, $password) {
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        // Prepare the SQL statement
+        
         $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
         $result = $this->database->query($sql);
 
-        // Check the result
+    
         if ($result) {
             $_SESSION['user_id'] = $result->insert_id;
             return true;
@@ -28,7 +28,7 @@ class User {
     }
     public function login($email, $password) {
 
-        // Prepare the SQL statement
+       
         $sql = "SELECT user_id, password FROM users WHERE email = '$email'";
         $result = $this->database->query($sql);
 
--- index.php
+++ index.php
@@ -45,7 +45,7 @@ require_once 'app/config/config.php';
         </div>
     </div>
 
-    <!-- Bootstrap JS (Optional) -->
+    <!-- Bootstrap JS  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
